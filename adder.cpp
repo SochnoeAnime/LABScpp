7e493f55b7ef7f1470a2a971d5ced96ccaa91e38
SochnoeAnime <52608100+SochnoeAnime@users.noreply.github.com>
2021-10-21 12:23:25 +0300

    Add files via upload

---
+++ adder.cpp
@@ -0,0 +1,24 @@
+/***********************************/
+/*				   */
+/* Code by: Khromov Egor Pavlovich */
+/* github.com/SochnoeAnime/LABScpp */
+/*		10.2021		   */
+/*				   */
+/***********************************/
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    double num1, num2;
+
+    cout << "Enter the first number" << endl;
+    cin >> num1;
+    cout << "Enter the second number" << endl;
+    cin >> num2;
+
+    cout << num1 << " + " << num2 << " = " << num1 + num2 << endl;
+
+    system("pause");
+    return 0;
+}
