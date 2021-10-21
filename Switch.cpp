0dc105360b09f972da1ead01894d4f4ee50b2882
SochnoeAnime <52608100+SochnoeAnime@users.noreply.github.com>
2021-10-21 13:40:59 +0300

    Add files via upload

---
+++ Switch.cpp
@@ -0,0 +1,70 @@
+/***********************************/
+/*				   */
+/* Code by: Khromov Egor Pavlovich */
+/* github.com/SochnoeAnime/LABScpp */
+/*		10.2021		   */
+/*				   */
+/***********************************/
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    for (int i = 1; i < 250; i++)
+    {
+        switch (i)
+        {
+        case 10:
+            cout << "Message #1\n";
+            break;
+        case 20:
+            cout << "Message #2\n";
+            break;
+        case 30:
+            cout << "Message #3\n";
+            break;
+        case 40:
+            cout << "Message #4\n";
+            break;
+        case 50:
+            cout << "Message #5\n";
+            break;
+        case 60:
+            cout << "Message #6\n";
+            break;
+        case 70:
+            cout << "Message #7\n";
+            break;
+        case 80:
+            cout << "Message #8\n";
+            break;
+        case 90:
+            cout << "Message #9\n";
+            break;
+        case 100:
+            cout << "Message #10\n";
+            break;
+        case 110:
+            cout << "Message #11\n";
+            break;
+        case 120:
+            cout << "Message #12\n";
+            break;
+        case 130:
+            cout << "Message #13\n";
+            break;
+        case 140:
+            cout << "Message #14\n";
+            break;
+        case 150:
+            cout << "Message #15\n";
+            break;
+        default:
+            break;
+        }
+    }
+
+    system("payse");
+    return 0;
+}
