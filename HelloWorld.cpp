dbfc1d2bbdb5f28b85728c940fe47cbd3a1454dc
SochnoeAnime <52608100+SochnoeAnime@users.noreply.github.com>
2021-10-21 12:16:58 +0300

    Add files via upload

---
+++ HelloWorld.cpp
@@ -0,0 +1,18 @@
+/************************************/
+/*									*/
+/*	Code by Khromov Egor Pavlovich	*/
+/*  github.com/SochnoeAnime/LABScpp */
+/*				10.2021				*/
+/*									*/
+/************************************/
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	cout << "Hello, World!" << endl;
+
+	system("pause");
+	return 0;
+}
