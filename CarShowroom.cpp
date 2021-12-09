c4e1946bf1ffb120d40df33c05b38eadfa204037
SochnoeAnime <52608100+SochnoeAnime@users.noreply.github.com>
2021-12-09 11:17:02 +0300

    Add files via upload

---
+++ CarShowroom.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+class Showroom {
+
+public:
+    Showroom() {}
+    string car;
+    virtual ~Showroom() {}
+    virtual void AddCar(string car) = 0;
+    virtual string GetCar() = 0;
+};
+
+class Showcase : public Showroom {
+
+public:
+    using Showroom::Showroom;
+    virtual ~Showcase()override {}
+    virtual void AddCar(string c)override;
+    virtual string GetCar()override;
+};
+
+class Storage : public Showroom {
+
+public:
+    using Showroom::Showroom;
+    virtual ~Storage()override {}
+    virtual void AddCar(string c)override;
+    virtual string GetCar()override;
+};
+
+
+int main() {
+    Showcase showcase;
+    Storage storage;
+
+    showcase.AddCar("Tesla Model S");
+    showcase.AddCar("Tesla Model X");
+    showcase.AddCar("Tesla Roadster");
+
+    storage.AddCar("Tesla Model Y");
+    storage.AddCar("Tesla Model 3");
+
+    cout << "In the showcase there are: " << showcase.GetCar() << endl;
+    cout << "In the storage there are: " << storage.GetCar() << endl;
+
+    return 0;
+}
+
+void Storage::AddCar(string c) {
+    car.empty() ? car += c : car += ", " + c;
+}
+
+string Storage::GetCar() {
+    return car;
+}
+
+void Showcase::AddCar(string c) {
+    car.empty() ? car += c : car += ", " + c;
+}
+
+string Showcase::GetCar() {
+    return car;
+}
